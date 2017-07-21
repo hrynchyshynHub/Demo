ba70d3223dae19c8ca28b3e4cb31fa79590aefe8
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2017-07-21 15:43:23 +0300

    refactored

---
+++ src/main/resources/db/migration/createDbSystem/create_Database.sql
@@ -0,0 +1,12 @@
+-- Database: learndb
+
+-- DROP DATABASE learndb;
+
+CREATE DATABASE learndb
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Ukrainian_Ukraine.1251'
+    LC_CTYPE = 'Ukrainian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
